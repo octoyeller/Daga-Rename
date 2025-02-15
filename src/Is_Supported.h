5722b9a1b7db1bf63145e8c3cc4206e598e31285
NotYourMagicBonkStick <media.octoyeller@gmail.com>
2025-02-15 19:36:35 +0100

    feat: readability

--- src/Is_Supported.h
+++ src/Is_Supported.h
@@ -14,7 +14,37 @@ bool Is_Supported (const std::string& target_ext) {
 
     std::string target = target_ext.substr(1, 5);
 
-    std::unordered_set<std::string> Supported_Files = {"JPEG", "jpeg", "jpg", "JPG", "TIFF", "tiff", "PNG", "png", "GIF", "gif", "BMP", "bmp", "WebP", "WEBP", "webp", "DNG", "CR2", "NEF", "ARW", "RW2", "ORF", "PEF", "SRF", "MRW", "RAF", "ERF", "SR2", "KDC", "DCR"};
+    std::unordered_set<std::string> Supported_Files = {
+        "JPEG",
+        "jpeg",
+        "jpg",
+        "JPG",
+        "TIFF",
+        "tiff",
+        "PNG",
+        "png",
+        "GIF",
+        "gif",
+        "BMP",
+        "bmp",
+        "WebP",
+        "WEBP",
+        "webp",
+        "DNG",
+        "CR2",
+        "NEF",
+        "ARW",
+        "RW2",
+        "ORF",
+        "PEF",
+        "SRF",
+        "MRW",
+        "RAF",
+        "ERF",
+        "SR2",
+        "KDC",
+        "DCR"
+    };
 
     if ( Supported_Files.find(target) != Supported_Files.end() ){
         return true;
